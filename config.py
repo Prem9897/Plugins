3b3e466e90b023ee85c9b005c7bda9b00200cf28
Prem mishra <118355053+Prem9897@users.noreply.github.com>
2022-11-22 23:00:32 +0530

    Update and rename ex_config.py to config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Development(Config):
     
     BOT_LIBRARY = "telethon"  # fill 'pyrogram' if you want pyrogram version of hellbot else leave it as it is.
 
-    DB_URI = "postgresql://xuajpuxl:k2vvRADJk0ggkxGf0AgTb9IJ2iwv5Mhu@mouse.db.elephantsql.com/xuajpuxl"  # A postgresql database url from elephantsql
+    DB_URI = "postgres://hbgsxcwd:lGZbEsaEds_7xIClUu1sMhjhm94i_TO2@mouse.db.elephantsql.com/hbgsxcwd"  # A postgresql database url from elephantsql
 
     HELLBOT_SESSION = "1BVtsOI4Bux92SQlZLQ9MFguLb_TEvWthwwd08nJtVMw1UxD5D9Livo-g-zDyqWiF77RMP3gWAiPelosZ4UPKbOkt4Gf5RWbbljYVMD3G5DFt6FJEd_HyQamS4uOxvWPdGw_xM2Reww9JrKuzcFJLmiH7XgtWheMWl1zUscLvz9llfXVLyEl-geXneqA3_dBD-LX0_8zzeixpxNvmjRmMbjijoxKnV0Ryr25Ki8MUoIy9SChwipYqhJwLRs5eMdFwyM0YDnmLIJ6ZKlzWZ6CEOTC9-6iKUNDJ6bk_Y_NaRCeqVc7z2IvIn8DyWeWieNVzedwiIHsQKj4lAjBfrqj1NStca6WOhWw="  # telethon or pyrogram string according to BOT_LIBRARY
 
